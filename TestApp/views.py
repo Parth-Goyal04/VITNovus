851fb355f0eb64a35a7dc93994ea3dfc479e1a52
parth <parth.goyal2023@vitstudent.ac.in>
2023-09-20 04:17:02 +0530

    commit 1

---
+++ TestApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "index.html")
+
+def supp(request):
+    return render(request, "supp.html")
+
+def page2(request):
+    return render(request, "page2.html")
+
+# Create your views here.
